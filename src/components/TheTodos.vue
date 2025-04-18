d8020d002fa1d3cc98bfdb9f18b41793dd044369
Omar Ibarhim <omarkholief01@gmail.com>
2025-04-18 23:01:30 +0200

    vue-projects

--- src/components/TheTodos.vue
+++ src/components/TheTodos.vue
@@ -35,7 +35,7 @@ const addTodo = () => {
     <form @submit.prevent="addTodo" class="max-w-lg my-5 m-auto">
       <div class="flex item-center rounded-2xl border overflow-hidden">
         <input class="flex-1 b-0 py-2.5 px-3.5 outline-0" type="text" placeholder="add todo" v-model="todoText" />
-        <button class="px-3.5 py-2.5 border-0 text-white bg-blue-600" type="submit">add</button>
+        <button class="px-4 py-2.5 border-0 text-white bg-blue-600" type="submit">add</button>
       </div>
     </form>
     <ul class="list-none max-w-lg mt-5 mb-5 m-auto">
